6c312c7425095b12d8b15dc227fa3df1d7e6ba96
Othman Djuliarso <37389043+dothmandev@users.noreply.github.com>
2021-11-22 12:58:24 -0800

    passing arguments between home and gallery fragments

--- app/src/main/java/com/zybooks/navdrawerfragments/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/zybooks/navdrawerfragments/ui/gallery/GalleryFragment.java
@@ -17,30 +17,17 @@ import com.zybooks.navdrawerfragments.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
-    private FragmentGalleryBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        return inflater.inflate(R.layout.fragment_gallery,container,false);
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView = view.findViewById(R.id.text_gallery);
+
+        int item = GalleryFragmentArgs.fromBundle(getArguments()).getGalleryItemId(); // get the value of the Destination Argument
+        textView.setText("Gallery Item: " + item); // set the textView appropriately
     }
 }
--- app/src/main/java/com/zybooks/navdrawerfragments/ui/home/HomeFragment.java
+++ app/src/main/java/com/zybooks/navdrawerfragments/ui/home/HomeFragment.java
@@ -5,49 +5,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.zybooks.navdrawerfragments.R;
-import com.zybooks.navdrawerfragments.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-    private FragmentHomeBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
+     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home,container,false);
-        //TODO: Sets the onClickListener for each button to the galleryItemButtonClick method with appropriate arguments
+//        TODO: Get a reference to each button object
+        Button galleryButton1 = root.findViewById(R.id.GalleryItem1);
+        Button galleryButton2 = root.findViewById(R.id.GalleryItem2);
+        Button galleryButton3 = root.findViewById(R.id.GalleryItem3);
 
-        View.OnClickListener onClickListener = itemView -> {
-
-            int selectedItemId = (int) itemView.getTag();
-
-        };
-
+        galleryButton1.setOnClickListener( v -> galleryItemButtonClick(v, 1) );
+        galleryButton2.setOnClickListener( v -> galleryItemButtonClick(v, 2) );
+        galleryButton3.setOnClickListener( v -> galleryItemButtonClick(v, 3) );
         return root;
     }
 
     public void galleryItemButtonClick(View view, int item){
         HomeFragmentDirections.ActionNavHomeToNavGallery action = HomeFragmentDirections.actionNavHomeToNavGallery();
-        //TODO: assign the appropriate item number value to the Destination Argument and navigate to the gallery page
-//        EditText itemNumber = (EditText) getView().findViewById(R.id.button);
-//
-        action.setAmount(amount);
-        Navigation.findNavController(view).navigate(action);
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+          action.setGalleryItemId(item);  // assign the appropriate item number value to the Destination Argument
+          Navigation.findNavController(view).navigate(action); // navigate to the gallery page
     }
 }
